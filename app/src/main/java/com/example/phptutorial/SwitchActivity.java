81830b36670ea436970a13adbba1e2536124930c
Vincent Fabron <152129153+ErwinJohannes@users.noreply.github.com>
2025-03-17 12:01:09 +0700

    JB

--- app/src/main/java/com/example/phptutorial/SwitchActivity.java
+++ app/src/main/java/com/example/phptutorial/SwitchActivity.java
@@ -1,4 +1,38 @@
 package com.example.phptutorial;
 
-public class SwitchActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SwitchActivity  extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_switch);
+
+        Button backswitch = findViewById(R.id.backswitch);
+        Button nextswitch = findViewById(R.id.nextswitch);
+
+        backswitch.setOnClickListener(this);
+        nextswitch.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.backswitch) {
+            Intent back = new Intent(SwitchActivity.this, IfelseActivity.class);
+            startActivity(back);
+        } else if (view.getId() == R.id.nextswitch) {
+           Intent next = new Intent(SwitchActivity.this,LoopActivity.class);
+            startActivity(next);
+        }
+
+    }
 }
