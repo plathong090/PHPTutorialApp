db529ac9f9bd5ae8f8f3708780b59e8cbda1601f
Vincent Fabron <152129153+ErwinJohannes@users.noreply.github.com>
2025-03-17 12:43:39 +0700

    Globe

--- app/src/main/java/com/example/phptutorial/GlobalsActivity.java
+++ app/src/main/java/com/example/phptutorial/GlobalsActivity.java
@@ -1,4 +1,38 @@
 package com.example.phptutorial;
 
-public class GlobalsActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GlobalsActivity  extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_superglobals);
+
+        Button backsuper = findViewById(R.id.backsuper);
+        Button nextsuper = findViewById(R.id.nextsuper);
+
+        backsuper.setOnClickListener(this);
+        nextsuper.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.backsuper) {
+            Intent back = new Intent(GlobalsActivity.this, ArrayActivity.class);
+            startActivity(back);
+        } else if (view.getId() == R.id.nextsuper) {
+            Intent next = new Intent(GlobalsActivity.this,RegexActivity.class);
+            startActivity(next);
+        }
+
+    }
 }
