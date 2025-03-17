e0ee8a5536f3cdf534715a95d562b218b928d029
Vincent Fabron <152129153+ErwinJohannes@users.noreply.github.com>
2025-03-17 13:00:31 +0700

    Globe

--- app/src/main/java/com/example/phptutorial/ArrayActivity.java
+++ app/src/main/java/com/example/phptutorial/ArrayActivity.java
@@ -1,4 +1,39 @@
 package com.example.phptutorial;
 
-public class ArrayActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ArrayActivity  extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_arrays);
+
+        Button backarray = findViewById(R.id.backarrays);
+        Button nextarray = findViewById(R.id.nextarrays);
+
+        backarray.setOnClickListener(this);
+        nextarray.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.backarrays) {
+            Intent back = new Intent(ArrayActivity.this, FunctionActivity.class);
+            startActivity(back);
+        } else if (view.getId() == R.id.nextarrays) {
+            Intent next = new Intent(ArrayActivity.this,GlobalsActivity.class);
+            startActivity(next);
+        }
+
+    }
 }
+
