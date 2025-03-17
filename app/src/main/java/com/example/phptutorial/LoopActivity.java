e769807f772687667f367a494c650cc342ff61e7
Vincent Fabron <152129153+ErwinJohannes@users.noreply.github.com>
2025-03-17 12:19:02 +0700

    FUnc

--- app/src/main/java/com/example/phptutorial/LoopActivity.java
+++ app/src/main/java/com/example/phptutorial/LoopActivity.java
@@ -1,4 +1,38 @@
 package com.example.phptutorial;
 
-public class LoopActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoopActivity  extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_loops);
+
+        Button backloops = findViewById(R.id.backloops);
+        Button nextloops = findViewById(R.id.nextloops);
+
+        backloops.setOnClickListener(this);
+        nextloops.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.backloops) {
+            Intent back = new Intent(LoopActivity.this, SwitchActivity.class);
+            startActivity(back);
+        } else if (view.getId() == R.id.nextloops) {
+            Intent next = new Intent(LoopActivity.this,FunctionActivity.class);
+            startActivity(next);
+        }
+
+    }
 }
